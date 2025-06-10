8cf7a592c80e13c817042221e802dc9c16242e88
Anujiina <aneku3185@gmail.com>
2025-06-10 19:57:40 +0800

    Update Soluton24.java

--- 24_LarryArray/Soluton24.java
+++ 24_LarryArray/Soluton24.java
@@ -1,5 +1,52 @@
-package 24_LarryArray;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-public class Soluton24 {
+class Result {
 
+ public static String larrysArray(List<Integer> A) {
+    int inversions = 0;
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = i + 1; j < A.size(); j++) {
+            if (A.get(i) > A.get(j)) {
+                inversions++;
+            }
+        }
+    }
+    return (inversions % 2 == 0) ? "YES" : "NO";
+}
+}
+
+public class Solution24 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            String[] ATemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> A = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int AItem = Integer.parseInt(ATemp[i]);
+                A.add(AItem);
+            }
+
+            String result = Result.larrysArray(A);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
