94dd672c2506afa4da962462621e331166e57d11
Anujiina <aneku3185@gmail.com>
2025-06-10 19:48:00 +0800

    Update Solution19.java

--- 19_IsThisBinary/Solution19.java
+++ 19_IsThisBinary/Solution19.java
@@ -1,5 +1,16 @@
-package 19_IsThisBinary;
+boolean checkBST(Node root) {
+        return isBSTNode(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
 
-public class Solution19 {
-
-}
+    boolean isBSTNode(Node root, int min, int max){
+        root2 = root;
+        this.root = root;
+        boolean isBstNode = false;
+        if(root == null){
+            return true;
+        }
+        if(root.data < min || root.data > max){
+            return false;
+        }
+        return (isBSTNode(root.left, min, root.data-1) && isBSTNode(root.right, root.data+1, max));
+    }
