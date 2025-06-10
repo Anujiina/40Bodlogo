145f2c2514f787b48eb8564f87fb5e5e53ceeab3
Anujiina <aneku3185@gmail.com>
2025-06-10 20:04:26 +0800

    Update Solution29.java

--- 29_ConnectedCell/Solution29.java
+++ 29_ConnectedCell/Solution29.java
@@ -1,5 +1,79 @@
-package 29_ConnectedCell;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+    public static int connectedCell(List<List<Integer>> matrix) {
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+        boolean[][] visited = new boolean[n][m];
+        int maxRegion = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 1 && !visited[i][j]) {
+                    int size = dfs(matrix, visited, i, j);
+                    maxRegion = Math.max(maxRegion, size);
+                }
+            }
+        }
+        return maxRegion;
+    }
+
+    public static int dfs(List<List<Integer>> matrix, boolean[][] visited, int i, int j) {
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return 0;
+        if (visited[i][j] || matrix.get(i).get(j) == 0)
+            return 0;
+        visited[i][j] = true;
+        int count = 1;
+        for (int row = -1; row <= 1; row++) {
+            for (int col = -1; col <= 1; col++) {
+                if (row != 0 || col != 0) {
+                    count += dfs(matrix, visited, i + row, j + col);
+                }
+            }
+        }
+        return count;
+    }
+}
+
 
 public class Solution29 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] matrixRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> matrixRowItems = new ArrayList<>();
+
+            for (int j = 0; j < m; j++) {
+                int matrixItem = Integer.parseInt(matrixRowTempItems[j]);
+                matrixRowItems.add(matrixItem);
+            }
+
+            matrix.add(matrixRowItems);
+        }
+
+        int result = Result.connectedCell(matrix);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
